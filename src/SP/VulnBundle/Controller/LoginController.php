b29cf2e1d3316dbc0618f3ad4113ea33ddcdb352
sprudnikov <sprudnikov@luxoft.com>
2016-12-07 13:32:20 +0600

    assets added

---
+++ src/SP/VulnBundle/Controller/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SP\VulnBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LoginController extends Controller
+{
+    public function indexAction()
+    {
+
+
+
+        return $this->render('VulnBundle:Login:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}
