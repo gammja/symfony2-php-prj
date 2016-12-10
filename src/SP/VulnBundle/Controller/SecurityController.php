017d58a74edb581183c6d1d019e531ea35311d40
sprudnikov <Roleshio44>
2016-12-10 19:37:40 +0600

    integrations with security component - in progress

---
+++ src/SP/VulnBundle/Controller/SecurityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SP\VulnBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        $auth = $this->get('security.authentication_utils');
+        return $this->render('VulnBundle:Login:login.html.twig', array(
+            'last_username' => $auth->getLastUsername(),
+            'error' => $auth->getLastAuthenticationError(),
+        ));
+    }
+}
